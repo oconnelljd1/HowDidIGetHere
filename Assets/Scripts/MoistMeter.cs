17f4844621ac646d1e6fdfe546f192ec1a12432e
geoffreypi <geoffreypi@gmail.com>
2022-04-01 14:44:07 -0300

    Update MoistMeter.cs
    Added in moisture meter desciptions

--- Assets/Scripts/MoistMeter.cs
+++ Assets/Scripts/MoistMeter.cs
@@ -19,7 +19,7 @@ public class MoistMeter : MonoBehaviour
         //moistLevelText = GameObject.Find("MoistLevel");
          moistLevelText = GetComponent <Text> ();
         
-        //string[] moistLevel = new string[] { "1", "2", "3", "4", "5", "6" "7", "8", "9", "10" };
+        //string[] moistLevel = new string[] { "Dewy", "Foggy", "Muggy", "Damp", "Dank", "Moist", "Wet", "Soggy", "Sopping", "Drenched" };
     }
 
     // Update is called once per frame
